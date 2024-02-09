7084426220aef586d570bb700be6eccf0be3cad0
RISK <81443404+RISK-alt@users.noreply.github.com>
2024-02-09 16:12:07 +0100

    Fix: DHT11 on 7 seg
    in progress

--- main.py
+++ main.py
@@ -2,155 +2,63 @@
 #include <TM1637.h>     //TM1637 LIBRARY
 
 
-__author__      = "risk"
+__author__      = "risk -alt"
 __copyright__   = "Copyright (c) 2024 RISK"
 
+#import librairies 
+import time
+import board
+import adafruit_dht
+import RPi.GPIO as GPIO
+import time
 
-  DHT dht(12 , DHT11); //DHT SENSOR PIN & SENSOR TYPE
-  TM1637 tm(9,8);      //TM1637 CLK & DIO PINs TO ARDUINO
+# Initialisez le dispositif dht avec la broche de données connectée à la broche 16 (GPIO 23) du Raspberry Pi :
+dhtDevice = adafruit_dht.DHT11(board.D23, use_pulseio=False)
 
-  byte S1   = 4; //SWITCH 1 TO SET BRIGHTNESS
-  byte S2   = 3; //SWITCH 2 TO CHANGE TEMPERATURE & HUMIDITY
-  byte switch1=0;
-  byte switch2=0;
-  byte COUNT=0;
-  byte SET=0;
-  byte GETSENSOR = 0;
-  byte SHIFT=0;
+# Inistialisez broches & GPIO
+#Segments 
+#PIN A : 19
+#PIN B : 20
+#PIN C : 21
+#PIN D : 22
+#PIN E : 23
+#PIN F : 24
+#PIN G : 25
+#PIN P : point
 
-void setup()
+Segment = {
+  0: (1,0,1,1,0,1,1,1,1,1), #A 
+  1: (1,1,1,1,1,0,0,1,1,1), #B
+  2: (1,1,0,1,1,1,1,1,1,1), #C
+  3: (1,0,1,1,0,1,1,0,1,1), #D
+  4: (1,0,1,0,0,0,1,0,1,0), #E
+  5: (1,0,0,0,1,1,1,0,1,1), #F
+  6: (0,0,1,1,1,1,1,0,1,1), #G
 
-  { 
-    Serial.begin(9600);
-    pinMode(S1,INPUT);
-    pinMode(S2,INPUT);
+while True:
+    try:
+        # Imprimer les valeurs via l'interface série
+        temperature_c = dhtDevice.temperature
+        temperature_f = temperature_c * (9 / 5) + 32
+        humidity = dhtDevice.humidity
+        print("Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(temperature_f, temperature_c, humidity))
 
-     dht.begin();
-     tm.begin();
-     tm.setBrightness(1);
-     delay(700);
-  }
-  
-void loop() 
-  { 
-    switch1=digitalRead(S1);
-    switch2=digitalRead(S2);    
- // Serial.print("COUNT :");    
- // Serial.println(COUNT);   
+      # Afficher les valeurs, sur l'afficheur 7 segments 
+        temperature_c = dhtDevice.temperature
+        temperature_f = temperature_c * (9 / 5) + 32
+        humidity = dhtDevice.humidity
+        print("Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(temperature_f, temperature_c, humidity))
 
-//SETTING DISPLAY BRIGHTNESS USING SWITCH 1
-  
- if(switch1==1)
-   {
-    COUNT++;
-    SET=1;
-   }
-    if(SET==1)
-      {
-       if(COUNT==1)
-         {
-          tm.setBrightness(2);
-          SET=0;
-         }
-       else
-       if(COUNT==2)
-         {
-          tm.setBrightness(3);
-          SET=0;
-         }
-      else
-      if(COUNT==3)
-        {
-         tm.setBrightness(4);
-         SET=0;
-        }
-      else
-      if(COUNT==4)
-        {
-         tm.setBrightness(5);
-         SET=0;
-        }
-      else
-      if(COUNT==5)
-        {
-         tm.setBrightness(6);
-         SET=0;
-        }
-     else
-     if(COUNT==6)
-       {
-        tm.setBrightness(7);
-        SET=0;
-       } 
-     else
-     if(COUNT==7)
-       {
-        tm.setBrightness(1);
-        COUNT=0;
-        SET=0;
-       }
-    }
+    except RuntimeError as error:
+        # Les erreurs sont assez fréquentes, les DHT sont difficiles à lire, passez à autre chose.
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    except Exception as error:
+        dhtDevice.exit()
+        raise error
 
-//SELECT WHAT YOU NEED TO SHOW ON DISPLAY USING SWITCH 2
-
-    if(switch2==1 && SHIFT==0)
-      {
-    // Serial.println("temperature"); 
-       GETSENSOR=0;
-      }  
-        
-    if(switch2==1 && SHIFT==1)
-      {
-    // Serial.println("humidity");
-       GETSENSOR=1;
-      }
-
-    if(switch2==1 && SHIFT==2)
-      {
-    // Serial.println("temperature & humidity");
-       GETSENSOR=2;
-      }      
-          
-     GET_DATA();
-  }
-  
-void GET_DATA() 
-  {   
-   if(GETSENSOR==0)
-     {
-      int temperature = dht.readTemperature();    
-   // Serial.println(temperature);     
-      tm.display("C", false,1,3);  //for 4 digit display 
-   // tm.display("C", false,false); //for more than 4 digit display  
-      tm.display(temperature, false, false); 
-      SHIFT=1;      
-      delay(1000); 
-     }
-   if(GETSENSOR==1)
-     {
-      int humidity = dht.readHumidity();   
-   // Serial.println(humidity);
-      tm.display("H", false,1,3); //for 4 digit display  
-   // tm.display("H", false,false); //for more than 4 digit display       
-      tm.display(humidity, false, false);  
-      SHIFT=2; 
-      delay(1000);
-     }
-     
-   if(GETSENSOR==2)
-     {
-      int temperature = dht.readTemperature();    
-   // Serial.println(temperature);     
-      tm.display("C", false,1,3);  //for 4 digit display 
-   // tm.display("C", false,false); //for more than 4 digit display  
-      tm.display(temperature, false, false);           
-      delay(1000); 
-      
-      int humidity = dht.readHumidity();   
-   // Serial.println(humidity);
-      tm.display("H", false,1,3); //for 4 digit display  
-   // tm.display("H", false,false); //for more than 4 digit display       
-      tm.display(humidity, false, false);  
+    time.sleep(2.0)
       SHIFT=0; 
       delay(1000);
      }
