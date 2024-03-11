3d7eeb27a0c933c4c7efcfb1475ebd73da4a5855
risk-alt <aldu6974@gmail.com>
2024-03-11 19:48:06 +0100

    MAINPATCH: WorkDHT11

--- main.py
+++ main.py
@@ -1,44 +1,30 @@
-#include "DHT.h"        //DHT SENSOR LIBRARY 
-#include <TM1637.h>     //TM1637 LIBRARY
-
-
-__author__      = "risk -alt"
-__copyright__   = "Copyright (c) 2024 RISK"
-
-#import librairies 
 import time
+import RPi.GPIO as GPIO
 import board
 import adafruit_dht
-import RPi.GPIO as GPIO
-import time
-
-# Initialisez le dispositif dht avec la broche de données connectée à la broche 16 (GPIO 23) du Raspberry Pi :
 dhtDevice = adafruit_dht.DHT11(board.D23, use_pulseio=False)
 
-# Inistialisez broches & GPIO
-#Segments 
-#PIN A : 19
-#PIN B : 20
-#PIN C : 21
-#PIN D : 22
-#PIN E : 23
-#PIN F : 24
-#PIN G : 25
-#PIN P : point
-point = 5 
-
+#PIN_A = 19
+#PIN_B = 20
+#PIN_C = 21
+#PIN_D = 22
+#PIN_E = 23
+#PIN_F = 24
+#PIN_G = 25
+point = 5
+comA1 = 6
+comA2 = 13
 Segment = {
-  0: (1,0,1,1,0,1,1,1,1,1), #A 
-  1: (1,1,1,1,1,0,0,1,1,1), #B
-  2: (1,1,0,1,1,1,1,1,1,1), #C
-  3: (1,0,1,1,0,1,1,0,1,1), #D
-  4: (1,0,1,0,0,0,1,0,1,0), #E
-  5: (1,0,0,0,1,1,1,0,1,1), #F
-  6: (0,0,1,1,1,1,1,0,1,1), #G
+    0: (1,0,1,1,0,1,1,1,1,1), #A
+    1: (1,1,1,1,1,0,0,1,1,1), #B
+    2: (1,1,0,1,1,1,1,1,1,1), #C
+    3: (1,0,1,1,0,1,1,0,1,1), #D
+    4: (1,0,1,0,0,0,1,0,1,0), #E
+    5: (1,0,0,0,1,1,1,0,1,1), #F
+    6: (0,0,1,1,1,1,1,0,1,1), #G
 }
-
 GPIO.setmode(GPIO.BCM)
-GPIO.setwarings(False) 
+GPIO.setwarnings(False)
 GPIO.setup(19, GPIO.OUT)
 GPIO.setup(20, GPIO.OUT)
 GPIO.setup(21, GPIO.OUT)
@@ -46,36 +32,53 @@ GPIO.setup(22, GPIO.OUT)
 GPIO.setup(23, GPIO.OUT)
 GPIO.setup(24, GPIO.OUT)
 GPIO.setup(25, GPIO.OUT)
-GPIO.setup(26, GPIO.OUT)
 GPIO.setup(point, GPIO.OUT)
-
-
---- GPIO maindef ---
+GPIO.setup(comA1, GPIO.OUT)
+GPIO.setup(comA2, GPIO.OUT)
+def affichage(num1, num2):
+    for i in range(0, 250):    
+        for i in range(0, 6):
+            GPIO.output((i+19), Segment[i][num1])
+        GPIO.output(comA1, LOW)
+        GPIO.output(comA2, HIGH)
+        time.sleep(0.01)
+        for i in range(0, 6):
+            GPIO.output((i+19), Segment[i][num2])
+        GPIO.output(comA1, HIGH)
+        GPIO.output(comA2, LOW)
+        time.sleep(0.01)
 while True:
-    try:
-        # Imprimer les valeurs via l'interface série
+    try:        
         temperature_c = dhtDevice.temperature
-        temperature_f = temperature_c * (9 / 5) + 32
-        humidity = dhtDevice.humidity
-        print("Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(temperature_f, temperature_c, humidity))
-
-      # Afficher les valeurs, sur l'afficheur 7 segments 
-        temperature_c = dhtDevice.temperature
-        temperature_f = temperature_c * (9 / 5) + 32
-        humidity = dhtDevice.humidity
-        print("Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(temperature_f, temperature_c, humidity))
-
+        temp = int(temperature_c)
+        ltemp = list(str(temp))
+        prm = ltemp[0]
+        scd = ltemp[1]
+        affichage(int(prm), int(scd))
+        GPIO.output(PIN_P, GPIO.LOW)
+        print("Temperature: ",temperature_c)
     except RuntimeError as error:
-        # Les erreurs sont assez fréquentes, les DHT sont difficiles à lire, passez à autre chose.
+        # Erreur parastie (passer a autre chose)
         print(error.args[0])
         time.sleep(2.0)
         continue
     except Exception as error:
         dhtDevice.exit()
         raise error
-
-    time.sleep(2.0)
-      SHIFT=0; 
-      delay(1000);
-     }
-  }
+    try:        
+        humidity = dhtDevice.humidity
+        hum = int(humidity)
+        lhum = list(str(hum))
+        hprm = lhum[0]
+        hscd = lhum[1]
+        affichage(int(hprm), int(hscd))
+        GPIO.output(PIN_P, GPIO.HIGH) # le point chiffre a virgule
+        print("Humidite: ",humidity)
+    except RuntimeError as error:
+        # Erreur / parasites passer a autre choses 
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    except Exception as error:
+        dhtDevice.exit()
+        raise error
