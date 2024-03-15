cd3f8c69fe6993e1bcede39d6c636615e2b63b6a
risk-alt <aldu6974@gmail.com>
2024-03-15 19:47:48 +0100

    add:value temp&humidity

--- python/DiscordWebhook.py
+++ python/DiscordWebhook.py
@@ -12,9 +12,13 @@ def alert(temp, humidity):
                     url="https://github.com/RISK-alt",
                     icon_url="https://cdn.discordapp.com/attachments/1216431298680324199/1216431366552551544/logo-white.png?ex=66005cfd&is=65ede7fd&hm=53913e65aa835bd837d4dd7e1172bd549893e0b1665529a8d0f392d1d8424a54&")
 
+    embed.add_embed_field(name="Température", value=temp)
+    embed.add_embed_field(name="Humidité", value=humidity)
+
     embed.set_image(url="https://cdn.discordapp.com/attachments/1216431298680324199/1217894977112379412/high-temperature-icon.webp?ex=6605b015&is=65f33b15&hm=89f1f88adb300246c6a43a0d5a71fbd421cf7335cbcc285a7764018a433665f1&")
 
     embed.set_footer(text="MESSAGE AUTOMATIQUE",
                     icon_url="https://cdn.discordapp.com/attachments/1216431298680324199/1217894977112379412/high-temperature-icon.webp?ex=6605b015&is=65f33b15&hm=89f1f88adb300246c6a43a0d5a71fbd421cf7335cbcc285a7764018a433665f1&")
+
     webhook.add_embed(embed)
     response = webhook.execute()
